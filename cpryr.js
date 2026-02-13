8d1915cec6bfee961784186e146a8467330ac8bf
Hayami, Hitoshi <18611767+hhayami@users.noreply.github.com>
2026-02-13 19:16:54 +0900

    Add files via upload

---
+++ cpryr.js
@@ -0,0 +1,7 @@
+<!--
+var now= new Date();
+if( now.getYear()<1900 ){ year=now.getYear()+1900 };
+month=now.getMonth()+1;
+document.write(year,"/",month,"/",now.getDate());
+document.write(" &copy;",year," Hitoshi Hayami")
+//-->
